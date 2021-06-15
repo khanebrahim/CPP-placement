36ee233b685995579e76aa5d6004fc83a460a6b3
EIkhan <ebrahimkhan5222@gmail.com>
2021-06-15 14:29:40 +0600

    added condition

---
+++ condition.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int savings;
+    cin>>savings;
+    
+    if(savings>5000){
+        cout<<"nasrin\n";
+
+    }
+    else{
+        cout<<"senthia\n";
+    }
+    return 0;
+}
