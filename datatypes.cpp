71ce5375fcdb020c8390fc68e49dabbf0497ae34
EIkhan <ebrahimkhan5222@gmail.com>
2021-06-15 12:25:18 +0600

    first commit

---
+++ datatypes.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    a=38;
+    cout<<"size of int "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"size of float "<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"size of char "<<sizeof(c)<<endl;
+
+    bool d;
+    cout<<"size of bool "<<sizeof(d)<<endl;
+
+    short int si;
+    long int Li;
+    cout<<"size of shortint "<<sizeof(si)<<endl;
+    cout<<"size of longint "<<sizeof(Li)<<endl;
+
+    return 0;
+}
