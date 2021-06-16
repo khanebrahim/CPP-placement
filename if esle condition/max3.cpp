7a11616d90919ffe407691b28317995f06459454
EIkhan <ebrahimkhan5222@gmail.com>
2021-06-16 19:42:44 +0600

    rearrage

--- if esle condition/max3.cpp
+++ if esle condition/max3.cpp
@@ -9,18 +9,18 @@ cin>>a>>b>>c;
 if(a>b){
 
 if(a>c){
-cout<<a<<endl;
+cout<<"a"<<endl;
 }
 else{
-cout<<c<<endl;
+cout<<"c"<<endl;
 }
 }
 else{
 if(b>c){
-cout<<b<<endl;
+cout<<"b"<<endl;
 }
 else{
-cout<<c<<endl;
+cout<<"c"<<endl;
 }
 }
 
