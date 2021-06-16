d1940bc8bdd99c050946b1bba8e606f2d19597a7
EIkhan <ebrahimkhan5222@gmail.com>
2021-06-16 17:03:32 +0600

    addes else if

---
+++ oddeven.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    if(n%2==0){
+        cout<<"even"<<endl;
+    }
+    else{
+        cout<<"odd"<<endl;
+    }
+    return 0;
+}
