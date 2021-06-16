db2818dfc1aca5ffc655d3c5a2cb38c09f96caac
EIkhan <ebrahimkhan5222@gmail.com>
2021-06-16 10:54:05 +0600

    condition

---
+++ logic.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int savings;
+    cin>>savings;
+
+    if(savings>5000){
+        cout<<"Nasrin\n";
+
+    }
+    else if(savings >2001){
+        cout<<"sinthia\n";
+}
+else{
+cout<<"Friends\n";
+}
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
