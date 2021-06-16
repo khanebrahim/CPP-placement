3410a6f82826f72986ce2e54344b76ef5f4028b8
EIkhan <ebrahimkhan5222@gmail.com>
2021-06-16 17:45:24 +0600

    nestedif

---
+++ nestedif.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cin>>x>>y;
+    if(x==y){
+        cout<<"both the numbers are equal"<<endl;
+    }
+    else{
+        if(x>y){
+            cout<<"x is greater than y"<<endl;
+        }
+        else{
+            cout<<"y is greater than x"<<endl;
+
+        }
+    }
+    return 0;
+}
